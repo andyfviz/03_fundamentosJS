985ea7e4b188fe3b59b294782065a658b45e7d1a
andyfviz <afloresv@centro.edu.mx>
2024-09-26 15:42:05 -0600

    Ejercicios

---
+++ 03_SUAVIZADO/main.js
@@ -0,0 +1,41 @@
+const element = document.getElementById("tester");
+let start;
+
+let mouseX = 0;
+let mouseY = 0;
+
+let smoothMouseX = 0;
+let smoothMouseY = 0;
+const COF = 0.02; // Coeficiente de Friccion
+
+function updateMouseCoords(eventData) {
+    mouseX = eventData.clientX;
+    mouseY = eventData.clientY;
+
+}
+
+document.addEventListener("mousemove", updateMouseCoords);
+
+
+
+
+function tick(timeStamp) {
+    if (start === undefined) {
+        start = timeStamp;
+    }
+    smoothMouseX += (mouseX - smoothMouseX) * COF;
+    smoothMouseY += (mouseY - smoothMouseY) * COF;
+
+    element.style.transform = `translate(${smoothMouseX}px, ${smoothMouseY}px)`;
+  
+  
+    
+    
+    requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
+
+
+
+
